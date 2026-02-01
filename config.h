29d7fcaabaa8a491a45dda4a6c956aac5f3e6e0f
xdevfaheem <xdevfaheem@gmail.com>
2026-02-02 00:17:38 +0530

    feat: applied bar height patch

--- config.h
+++ config.h
@@ -5,6 +5,7 @@ static const unsigned int borderpx  = 4;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const int user_bh            = 2;        /* 2 is the default spacing around the bar's font */
 static const char *fonts[]          = { "JetBrainsMono NFM:size=18:style=Bold:antialias=true:autohint=true" };
 static const char dmenufont[]       = "JetBrainsMono NFM:size=16:style=Bold:antialias=true:autohint=true";
 static const char col_gray1[]       = "#222222";
