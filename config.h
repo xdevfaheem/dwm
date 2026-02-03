6dfdfff8d29fcb5016cb686b0da477b94cde2f4d
xdevfaheem <xdevfaheem@gmail.com>
2026-02-03 18:09:58 +0530

    refactor: minor tweak
    this fixes my long standing issue with status bar being huge, can't find
    a reduce it (actually i was tweaking dpi, tho it was kinda ok but it
    affects everywhere like browser, etc) up until i just reduced the font
    size. bruh... moment

--- config.h
+++ config.h
@@ -1,12 +1,12 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 4;        /* border pixel of windows */
+static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const int user_bh            = 2;        /* 2 is the default spacing around the bar's font */
-static const char *fonts[]          = { "JetBrainsMono NFM:size=18:style=Bold:antialias=true:autohint=true" };
+static const int user_bh            = 5;        /* 2 is the default spacing around the bar's font */
+static const char *fonts[]          = { "JetBrainsMono NFM:size=12:antialias=true:autohint=true" };
 static const char dmenufont[]       = "JetBrainsMono NFM:size=16:style=Bold:antialias=true:autohint=true";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
